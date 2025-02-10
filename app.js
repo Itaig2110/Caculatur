eaa0f9ce49d347f77f89b2813aa919abb66cb14b
Itai G <Itaig2110@gmail.com>
2025-02-10 22:18:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+
+
+
+app.get('/calculator',(req,res)=>{
+    res.sendFile(path.join(__dirname,"app.html"));
+});
+app.use((req,res)=>{
+    res.status(404).send("Page Not Found ! ");
+});
+module.exports = app;

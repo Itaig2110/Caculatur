aa0b2884dd99eb2f3459dbb7f3e8352dca639894
Itai G <Itaig2110@gmail.com>
2025-02-10 23:11:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 
 app.get('/calculator',(req,res)=>{
-    res.sendFile(path.join(__dirname,"app.html"));
+    res.sendFile(path.join(__dirname,"index.html"));
 });
 app.use((req,res)=>{
     res.status(404).send("Page Not Found ! ");
